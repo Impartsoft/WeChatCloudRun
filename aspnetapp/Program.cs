d50002d8c426c4b280da71e4bdfb67e38b532dff
Harry <364424187@qq.com>
2023-11-04 17:42:46 +0800

    add websocket2

--- aspnetapp/Program.cs
+++ aspnetapp/Program.cs
@@ -1,11 +1,36 @@
 using aspnetapp;
+using SignalRChat.Hubs;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddDbContext<CounterContext>();
+builder.Services.AddSignalR();
 
 var app = builder.Build();
+
+
+//app.Use(async (context, next) =>
+//{
+//    if (context.Request.Path == "/ws")
+//    {
+//        if (context.WebSockets.IsWebSocketRequest)
+//        {
+//            using var webSocket = await context.WebSockets.AcceptWebSocketAsync();
+//            await MyWebSocket.Echo(webSocket);
+//        }
+//        else
+//        {
+//            context.Response.StatusCode = StatusCodes.Status400BadRequest;
+//        }
+//    }
+//    else
+//    {
+//        await next(context);
+//    }
+
+//});
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
@@ -26,5 +51,6 @@ app.UseRouting();
 app.UseAuthorization();
 app.MapControllers();
 app.MapRazorPages();
+app.MapHub<ChatHub>("/chatHub");
 
 app.Run();
